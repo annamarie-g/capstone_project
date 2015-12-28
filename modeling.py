45243024af8029d22a15fb3e567a8db0986b4dd9
sallamander <ssall@alumni.nd.edu>
2015-12-28 08:28:56 +0000

    svr

--- modeling.py
+++ modeling.py
@@ -12,7 +12,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.cross_validation import train_test_split 
 from sklearn.grid_search import GridSearchCV
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC, SVR, LinearSVR
 
 
 def create_collocations_from_trainingset(series):
@@ -60,12 +60,47 @@ def gradient_boosting(X_train, y_train):
     gb.fit_transform(X_train, y_train) 
     return gb 
 
+def svc_rbf(X_train, y_train_clf):
+    est = SVC(kernel='rbf', random_state = 42)
+    clf = GridSearchCV(est, parameters, cv=2, n_jobs=-1)
+    clf.fit(X_train, y_train_clf)
+    print 'Best SVC estimator:'
+    print clf.best_estimator_
+    print 'Best accuracy score:'
+    print clf.best_score_ 
+    return clf.best_estimator_
+
+def svr_rbf(X_train, y_train):
+    parameters = {'C': [0.0001, 0.1, 100], 'loss':['l1', 'l2']}
+    est = SVR(kernel='rbf')
+    reg = GridSearchCV(est, parameters, cv=2, n_jobs=-1)
+    reg.fit(X_train, y_train)
+    print 'Best SVR estimator:'
+    print reg.best_estimator_
+    print 'Best R^2  score:'
+    print reg.best_score_ 
+    return reg.best_estimator_
+
 def linear_svc(X_train, y_train_clf):
     #linear support vector classification
     parameters = {'C': [0.0001, 0.1, 100], 'loss':['l1', 'l2']}
     est = LinearSVC()
     clf = GridSearchCV(est, parameters, cv=2, n_jobs = -1) 
     clf.fit(X_train, y_train_clf)
+    print 'best model:'
+    print clf.best_estimator_
+    print 'best score:' 
+    print clf.best_score_
+    print 'grid scores:' 
+    print clf.grid_scores_     
+    return clf.best_estimator_
+
+def linear_svr(X_train, y_train):
+    #linear support vector classification
+    parameters = {'C': [0.0001,.005, 0.1, 100], 'loss':['l1', 'l2']}
+    est = LinearSVR()
+    clf = GridSearchCV(est, parameters, cv=2, n_jobs = -1) 
+    clf.fit(X_train, y_train)
     print 'best model:'
     print clf.best_estimator_
     print 'best score:' 
@@ -109,26 +144,32 @@ def create_featurespace(df):
     #add total text length as feature
     df['total_text_length'] = df['total_text'].map(len)
     #combine matrices 
-    total_mat = build_feature_matrix((text_mat, title_mat,  np.array(df[['num_attributes', 'num_images', 'total_text_length']])))
+    total_mat = build_feature_matrix((text_mat, cat_dummies, title_mat,  np.array(df[['num_attributes', 'num_images', 'total_text_length']])))
     return total_mat 	
 
 if __name__=='__main__':	
     df, target = get_data()
     total_mat = create_featurespace(df)
 #    total_mat = reduce_dimensions(total_mat, n_topics=10000)
+
     X_train, X_test, y_train, y_test = train_test_split(total_mat, target, test_size = 0.3)
 
     #create age group on y_train and y_test 
     y_train_clf = create_age_groups(y_train)
     y_test_clf = create_age_groups(y_test)	
-'''
-    svm_clf = linear_svc(X_train, y_train_clf)
-    svm_clf.score(X_test, y_test_clf)
 
-    gb = gradient_boosting(X_train.todense(), y_train)
-    print 'Gradient Boosted Model:'
-    print gb.score(X_train, y_train) 
+    svm_reg = linear_svr(X_train, y_train)
+    svm_reg.score(X_test, y_test)
+
+    reg = svr_rbf(X_train, y_train)
+    print 'Best svc classifier accuracy:' 
+    print reg.score(X_test, y_test) 
+
 '''
+
+    clf = svc_rbf(X_train, y_train_clf)
+    print 'Best svc classifier accuracy:' 
+    print clf.score(X_test, y_test_clf) 
 
     rfc = random_forest_classifier(X_train, y_train_clf)
     rfc.transform(X_train, y_train_clf)
@@ -139,4 +180,11 @@ if __name__=='__main__':
     rfr.transform(X_train, y_train)
     print "Best Random Forest Regressor R^2:"
     print rfr.score(X_test, y_test)
+
+    svm_clf = linear_svc(X_train, y_train_clf)
+    svm_clf.score(X_test, y_test_clf)
+
+    gb = gradient_boosting(X_train.todense(), y_train)
+    print 'Gradient Boosted Model:'
+    print gb.score(X_train, y_train) 
 '''
