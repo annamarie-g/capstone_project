3d4841e447112444a243952a4ec81f4383d2f67a
sallamander <ssall@alumni.nd.edu>
2015-12-30 19:36:01 +0000

    eda txt

--- eda.py
+++ eda.py
@@ -10,9 +10,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 def print_top_tokens(model, feature_names, n_top_words):
    for topic_idx, topic in enumerate(model.components_):
-	print ("Topic #%d:" % topic_idx)
-	print (' '.join([feature_names[i] for i in topic.argsort()[:-n_top_words -1:-1]]))
-	print ()
+	with open('results.txt', 'wb') as fid:
+	   fid.("Topic #%d:" % topic_idx)
+	   fid.(' '.join([feature_names[i] for i in topic.argsort()[:-n_top_words -1:-1]]))
 
 def get_data():
     with open('dataframe_for_eda.pkl', 'rb') as fid:
